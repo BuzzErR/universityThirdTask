7f7074696f8ae8ccbf5dbd9fc030bb5be3f6e705
BuzzErR <new.mikhailforwork@gmail.com>
2021-12-14 22:06:50 +0300

    release

--- bot.py
+++ bot.py
@@ -1,8 +1,21 @@
 import telebot
 
-f = open('config.txt', 'r')
-data = f.read()
+with open('config.txt', 'r') as f:
+    data = f.read()
 token, father_id = data.split()
-bot = telebot.TeleBot(token, threaded=False)
 
-bot.send_message(father_id, 'Hello, father')
+with open('bot_buffer.txt', 'r') as f:
+    data = f.read()
+    num_of_failed_tests = len(data.split('\n')) - 1
+    
+if len(data) == 0:
+    message = 'Все в порядке'
+else:
+    message = f'number of failed tests {num_of_failed_tests}\n' + data
+
+
+bot = telebot.TeleBot(token, threaded=False)
+bot.send_message(father_id, message)
+
+with open('bot_buffer.txt', 'w') as f:
+    f.write('')
--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,7 @@
-import main
 import unittest
 import random
 import functools
-
+import main
 
 def generate_random_data():
     result = []
@@ -17,27 +16,52 @@ class TestMethods(unittest.TestCase):
 
     def test_min(self):
         # print(self.numbers)
-        self.assertEqual(min(self.numbers), main.find_min(self.numbers))
+        try:
+            self.assertEqual(min(self.numbers), main.find_min(self.numbers))
+        except AssertionError as e:
+            with open('bot_buffer.txt', 'a') as f:
+                f.write(str(e) + ' test_min\n')
+            raise Exception(e)
 
     def test_max(self):
         # print(self.numbers)
-        self.assertEqual(max(self.numbers), main.find_max(self.numbers))
+        try:
+            self.assertEqual(max(self.numbers) + 5, main.find_max(self.numbers))
+        except AssertionError as e:
+            with open('bot_buffer.txt', 'a') as f:
+                f.write(str(e) + ' test_max\n')
+            raise Exception(e)
+        
+            
 
     def test_sum(self):
         # print(self.numbers)
-        self.assertEqual(sum(self.numbers), main.sum_of_numbers(self.numbers))
+        try:
+            self.assertEqual(sum(self.numbers), main.sum_of_numbers(self.numbers))
+        except AssertionError as e:
+            with open('bot_buffer.txt', 'a') as f:
+                f.write(str(e) + ' test_sum\n')
+            raise Exception(e)
+            
 
     def test_multiplication_no_overflow(self):
         # print(self.numbers)
-        self.assertEqual(functools.reduce((lambda x, y: x * y), self.numbers),
-                         main.multiplication_of_numbers(self.numbers))
+        try:
+            self.assertEqual(functools.reduce((lambda x, y: x * y), self.numbers),
+                            main.multiplication_of_numbers(self.numbers))
+        except AssertionError as e:
+            with open('bot_buffer.txt', 'a') as f:
+                f.write(str(e) + ' test_multiplication_no_overflow\n')
+            raise Exception(e)
 
     def test_import_of_data(self):
         # возможно предусмотреть создание уникального файла, но в задании подобного не требуется
         with open('generator_for_tests.txt', 'w') as f:
             for item in self.numbers:
                 f.write("%s " % item)
-            f.close()
-        self.assertEqual(self.numbers, main.get_numbers_from_file('generator_for_tests.txt'))
-
-
+        try:
+            self.assertEqual(self.numbers, main.get_numbers_from_file('generator_for_tests.txt'))
+        except AssertionError as e:
+            with open('bot_buffer.txt', 'a') as f:
+                f.write(str(e) + ' test_import_of_data\n')
+            raise Exception(e)
