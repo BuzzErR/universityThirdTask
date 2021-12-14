10b65fbdba080c7317b04100dd206f4601173340
BuzzErR <new.mikhailforwork@gmail.com>
2021-12-14 22:16:59 +0300

    no buffer error fixed

--- bot.py
+++ bot.py
@@ -3,10 +3,12 @@ import telebot
 with open('config.txt', 'r') as f:
     data = f.read()
 token, father_id = data.split()
-
-with open('bot_buffer.txt', 'r') as f:
-    data = f.read()
-    num_of_failed_tests = len(data.split('\n')) - 1
+try:
+    with open('bot_buffer.txt', 'r') as f:
+        data = f.read()
+        num_of_failed_tests = len(data.split('\n')) - 1
+except FileNotFoundError:
+    data = ''
     
 if len(data) == 0:
     message = 'Все в порядке'
